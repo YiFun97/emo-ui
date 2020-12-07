b2c9573d2a71bbb1ddd9a87177ac96b97b64b8f4
yifun97 <yifun97@github.com>
2020-12-07 23:33:37 +0800

    ✨feat(所有模块): 项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>我的第一个组件</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
